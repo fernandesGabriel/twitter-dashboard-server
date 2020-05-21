47cdfa6d3934ef45691bd966327fae963f08b370
Gabriel Fernandes <gabriel_fernandes1993@hotmail.com>
2020-05-21 14:12:23 +0200

    Ini socketio

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 'use strict';
 
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
 const app = express();
 
+// define routes
 app.get('/', (req, res) => {
-  res.send('Hello Backend');
+  res.send({ response: 'Twitter Dashboard Backend' }).status(200);
 });
 
-app.listen(8080)
+// boot server and socket
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let interval;
+
+const getApiAndEmit = socket => {
+  socket.emit('FromAPI', new Date());
+};
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  if (interval) {
+    clearInterval(interval);
+  }
+  
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+    clearInterval(interval);
+  });
+});
+
+// listen for requests
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+});
