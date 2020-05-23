01e587bb59149b59a080908280bacf24ac38c9de
Gabriel Fernandes <gabriel_fernandes1993@hotmail.com>
2020-05-23 18:47:37 +0200

    Simplify test socket output

--- src/controllers/socket/feed.js
+++ src/controllers/socket/feed.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.getSocket = (req, res) => {  
+exports.getSocket = (req, res) => {
   const socketConfig = { path: req.route.path, serveClient: false };
   const io = require('socket.io')(req.connection.server, socketConfig);
 
