37a261b0e908f50617e3133799a64a9faa17bd60
Gabriel Fernandes <gabriel_fernandes1993@hotmail.com>
2020-05-24 21:31:29 +0200

    Add cors middleware

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@
 
 const app = require('express')();
 const http = require('http').createServer(app);
+const cors = require('cors');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(cors());
 app.use(require('./controllers'));
 
 http.listen(app.get('port'), () => {
