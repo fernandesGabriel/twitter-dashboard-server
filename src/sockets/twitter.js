f6897d8ff1dba36d70548c7f0c6b9e48ce1a6e1f
Gabriel Fernandes <gabriel_fernandes1993@hotmail.com>
2020-05-24 20:55:01 +0200

    Listen for stream tracking subject

--- src/sockets/twitter.js
+++ src/sockets/twitter.js
@@ -1,26 +1,42 @@
 'use strict';
 
+
+const TRACK_CHANNEL = 'twitter-track-for';
 const STREAM_CHANNEL = 'twitter-stream';
 
 module.exports = (socket) => {
   const twitter = new require('twitter')({
-      consumer_key: process.env.TWITTER_CONSUMER_KEY,
-      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-      access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
   });
 
-  twitter.stream('statuses/filter', { track: 'JavaScript' }, (stream) => {
-    stream.on('data', (tweet) => {
-      emit(tweet);
-    });
+  let twitterStream;
 
-    stream.on('error', (error) => {
+  socket.on(TRACK_CHANNEL, (from, track) => {
+    if (twitterStream) 
+      twitterStream.destroy();
+
+    if (track)
+      stream(track);
+  });
+  
+  const stream = (track) => {
+    twitter.stream('statuses/filter', { track: track }, (stream) => {
+      stream.on('data', (data) => {
+        emit(data);
+      });
+
+      stream.on('error', (error) => {
         console.log(error);
-    });
-  });
+      });
 
-  const emit = (message) => {
-    socket.emit(STREAM_CHANNEL, message);
+      twitterStream = stream;
+    });
+  }
+
+  const emit = (data) => {
+    socket.emit(STREAM_CHANNEL, data);
   }
 }
